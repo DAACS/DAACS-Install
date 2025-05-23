5cdf67a7e264c229492f75c53c7dca1513c44bc5
moomoo-dev <moomoodevelopment@gmail.com>
2025-05-23 19:01:13 +0000

    adding config to start up

--- DAACS-Install-Defaults/DAACS-Mongo/Replica/docker/run.sh
+++ DAACS-Install-Defaults/DAACS-Mongo/Replica/docker/run.sh
@@ -14,4 +14,4 @@
 # cat /home/mongossl.key /home/mongossl.cert > /home/mongossl.pem
 # mongod --config /home/mongod.conf --replSet myReplicaSet --setParameter tlsUseSystemCA=true 
 # mongod --replSet myReplicaSet --setParameter tlsUseSystemCA=true 
-mongod
+mongod --config /home/mongod.conf
